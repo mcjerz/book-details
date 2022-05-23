af64126be0a7a783aa2e6ae59796a09673421f20
alyson albasi <aly@mcjerz.net>
2022-05-23 12:07:13 -0400

    graphql-java.com tutorial

--- src/main/java/com/graphqljava/tutorial/bookdetails/BookDetailsApplication.java
+++ src/main/java/com/graphqljava/tutorial/bookdetails/BookDetailsApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookDetailsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookDetailsApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookDetailsApplication.class, args);
+    }
 
 }
